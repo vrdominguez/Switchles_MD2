759841fe130b237a9e95ae89494fd4ed8ba2df34
Víctor R. Rodríguez Domínguez <l3st4t.85@gmail.com>
2018-08-15 23:20:05 +0200

    Includio cambio solo de frecuencia

--- README.md
+++ README.md
@@ -1,16 +1,49 @@
-# Switchless MD2
-Cambio de region para MegaDrive 2 sin interruptores, se utiliza el botón de reset de la propia consola
+# Mod cambio de región sin interruptores para Mega Drive II
+Control de cambio de region (Europa, USA, Japon), o de frecuencia (50/60Hz) de la Mega Drive 2, utilizando unicamente el boton de reset de la misma consola. Segun el tiempo que se pulsa el boton:
+  - **Menos de 1 segundo:** Reset
+  - **Entre 1 y 2 segundos:** Cambio entre 50Hz y 60Hz
+  - **Mas de 2 segundos:** Cambio ciclico de Region: EUR->USA->JAP->EUR
 
 ## Dependencias
 
 ### Librería LowPower
 Debemos descargarla de su proyecto oficial en Github ([rocketscream/Low-Power](https://github.com/rocketscream/Low-Power)) y situarla en el directorio de librerías (lib) dentro de la carpeta LowPower
 
-## Funcionamiento
-El arduino pasa a tomar el conrol del reset de la consola, permitiéndo su uso para controlarlas regiones de la misma, teniendo 2 posibles acciones:
+## Regiones diponibles
 
-  - Si pulsamos reset durante menos de 2 segundos, la consola se reiniciará de forma normal
-  - Si pulsamos reset durante 2 segundos o más, la consola rotará entre las siguientes regiones
-    - EUR: No japonesa a 50Hz (por defecto al encender)
-    - USA: No japonesa a 60Hz (NTSC)
-    - JAP: Japonesa a 60Hz
+### Europa
+Esta es la region PAL europea de la Sega Mega Drive, se corresponde a la combinacion de refresco a **50Hz** e idioma **no japonés**. Se puede llegar a ella mediante cambios de región (pulsacion de 2 o más segundos) o cambiando la frecuencia desde la región _USA_
+
+### Japón
+Es la región de las Mega Drive asiáticas (PAL Japón), se corresponde a la combinación de refresco a **60Hz** e idioma **japonés**. Se puede llegar a ella mediante cambios de región (pulsacion de 2 o más segundos) o cambiando la frecuencia desde la región _custom_
+
+### USA
+Es la región de las Mega Drive americanas (NTSC), se corresponde a la combinación de refresco a **60Hz** e idioma **no japonés**. Se puede llegar a ella mediante cambios de región (pulsacion de 2 o más segundos) o cambiando la frecuencia desde la región _Europa_
+
+### Región personalizada o custom
+No es una región existente en las consolas vendidas, es el resultdo de combinar el refresco a **50Hz** e idioma **japonés**. Solamente se puede llegar a ella cambiando la frecuencia desde la región _japonesa_, pero no desde el cambio de región, ya que no es una región real de la consola. 
+
+**AVISO:** Al no ser una región real de la consola, es posible que se produzcan errores en algunos juegos. Solo se incluye porque es accesible mediante el mod con interruptores
+
+## Led de Mega Drive
+Al reemplazar el led de la Mega Drive II por un LED RGB, este nos indicará el estado de configuración de la consola, según los diferentes colores
+
+### Azul
+Se corresponde con la región **Europa**, nos inidca que la consola está configurada como
+  - **Frecuencia de refresco:** 50Hz
+  - **Idioma**: No japonés
+
+### Morado
+Se corresponde con la región **personalizada** o **custom**, nos inidca que la consola está configurada como
+  - **Frecuencia de refresco:** 50Hz
+  - **Idioma**: Japonés
+
+### Rojo
+Se corresponde con la región **Japón**, nos inidca que la consola está configurada como
+  - **Frecuencia de refresco:** 60Hz
+  - **Idioma**: Japonés
+
+### Verde
+Se corresponde con la región **USA**, nos inidca que la consola está configurada como
+  - **Frecuencia de refresco:** 60Hz
+  - **Idioma**: No japonés
--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,150 @@
-/* ****************************************************************************
- * Control de cambio de region (idioma japones/otro, frecuenta 50/60Hz) para
- * la SEGA Mega Drive 2, mediante el propio botón de reset de la consola
+/** ***************************************************************************
+ * Control de cambio de region (Europa, USA, Japon), o de frecuencia (50/60Hz)
+ * de la Mega Drive 2, utilizando unicamente el boton de reset de la misma
+ * consola. Segun el tiempo que se pulsa el boton:
+ *    - Menos de 1 segundo: Reset
+ *    - Entre 1 y 2 segundos: Cambio entre 50Hz y 60Hz
+ *    - Mas de 2 segundos: Cambio ciclico de Region: EUR->USA->JAP->EUR
  * ************************************************************************* */
 #include <Arduino.h>
 #include "LowPower.h"
 
 // --- CONFIGURACION DE PINES CONECTADOS A LA CONSOLA -------------------------
-int resetInput = 2; // Controla la interrupcion del boton reset
-int pinJAP = 12;    // Controla el idioma de la consola
-int pinNTSC = 13;   // Controla la frecuencia de refresco
-int pinReset = 8;   // Controla el reseteo
+int resetInput =  2; // Controla la interrupcion del boton reset
+int pinJAP     = 12; // Controla el idioma de la consola
+int pinNTSC    = 13; // Controla la frecuencia de refresco
+int pinReset   =  8; // Controla el reseteo
 
 // --- SALIDAS PARA CONTROLAR LA ILUMINACION DEL LED --------------------------
 int rled = 11; // Pin rojo de led RGB
 int gled = 10; // Pin verde de led RGB
 int bled = 9;  // Pin azul de led RGB
 
+// --- COLORES LED ------------------------------------------------------------
+int jap[3] = {255,000,  0}; // Rojo para Japon
+int usa[3] = {  0,255,  0}; // Verde para USA
+int eur[3] = {  0,  0,255}; // Azul para Europa
+int cus[3] = {255, 51,255}; // Morado para "personalizada"
+
 // --- OTRAS VARIABLES --------------------------------------------------------
-String region = "EUR";   // Guarda la region actual
-int inicioPulsacion = 0; // Guarda milisesgundos de inicio de pulsacion
+String region       = "EUR"; // Guarda la region actual
 
 // --- FUNCIONES --------------------------------------------------------------
-void colorearLed ( int r, int g, int b ) {
-  analogWrite(rled,r);
-  analogWrite(gled,g);
-  analogWrite(bled,b);
+/**
+ * Establece el color del led rgb
+ *
+ * @param int[] array con los valores de RGB (3 valores, entre 0 y 255)
+ * @return void
+ */
+void colorearLed ( int rgb[3] ) {
+  analogWrite(rled,rgb[0]);
+  analogWrite(gled,rgb[1]);
+  analogWrite(bled,rgb[2]);
 }
 
-void parpadearLED() {
-  for( int i = 0; i < 4; i++) {
-    colorearLed(253,106,3);
-    delay(100);
-    colorearLed(0,0,0);
-    delay(100);
+/**
+ * Apaga el led RGB
+ *
+ * @param ---
+ * @return void
+*/
+void apagarLed() {
+  analogWrite(rled,0);
+  analogWrite(gled,0);
+  analogWrite(bled,0);
+}
+
+/**
+ * Hace parpadear el led RGB, en el color, frecuencia y repeticiones indicadas
+ *
+ * @param int[] color con los valores de RGB (3 valores, entre 0 y 255)
+ * @param int tiempo milisegundos de encendido/apagado
+ * @param int repeticiones numero de ciclos de apagado y encendido
+ * @return void
+ */
+void parpadearLED(int color[3], int tiempo=50, int repeticiones=5) {
+  for( int i = 0; i < repeticiones; i++) {
+    apagarLed();
+    delay(tiempo);
+    colorearLed(color);
+    delay(tiempo);
   }
 }
 
+/**
+ * Establece la region como Europa (no japones, 50Hz) y pone el led en azul
+ *
+ * @param ---
+ * @return void
+ */
 void setEUR() {
-  // Idioma no japones, 50Hz
   digitalWrite(pinJAP, HIGH);
   digitalWrite(pinNTSC, LOW);
 
-  // Led azul
-  for ( int intensidad = 0; intensidad <= 255; intensidad++ ) {
-    colorearLed(0,0,intensidad);
-    delay(5);
-  }
+  parpadearLED(eur,125,2);
 
-  // Guardamos la region
   region = "EUR";
 }
 
+/**
+ * Establece la region como USA (no japones, 60Hz) y pone el led en verde
+ *
+ * @param ---
+ * @return void
+ */
 void setUSA() {
-  // Idioma no japones, 60Hz
   digitalWrite(pinJAP, HIGH);
   digitalWrite(pinNTSC, HIGH);
   
-  // Led verde
-  for ( int intensidad = 0; intensidad <= 255; intensidad++ ) {
-    colorearLed(0,intensidad,0);
-    delay(5);
-  }
-  
-  // Guardamos la region
+  parpadearLED(usa);
+
   region = "USA";
 }
 
+/**
+ * Establece la region como Japon (japones, 60Hz) y pone el led en rojo
+ *
+ * @param ---
+ * @return void
+ */
 void setJAP() {
-  // Idioma no japones, 60Hz
   digitalWrite(pinJAP, LOW);
   digitalWrite(pinNTSC, HIGH);
   
-  // Led en rojo, por la bandera de Japon
-  for ( int intensidad = 0; intensidad <= 255; intensidad++ ) {
-    colorearLed(intensidad,0,0);
-    delay(5);
-  }
+  parpadearLED(jap);
 
-  // Guardamos la region
   region = "JAP";
 }
 
-void cambiarRegion() {
-  parpadearLED();
+/**
+ * Establece la region como "custom" (japones, 50Hz) y pone el led en morado.
+ * Solamente se puede llegar a esta combinacion haciedo un cambio de frecuencia
+ * desde la region "Japon", en lugar de cambiar de region. No es una de las
+ * regiones existentes en la consola
+ *
+ * @param ---
+ * @return void
+ */
+void setCustom() {
+  // Idioma no japones, 60Hz
+  digitalWrite(pinJAP, LOW);
+  digitalWrite(pinNTSC, LOW);
   
+  // Led en rojo, por la bandera de Japon
+  parpadearLED(cus,125,2);
+
+  // Guardamos la region
+  region = "CUS";
+}
+
+/**
+ * Configura la region de la consola en una de las 3 existentes (Europera, USA
+ * o Japon) de forma ciclica: EUR -> USA -> JAPON -> EUR
+ *
+ * @param ---
+ * @return void
+ */
+void cambiarRegion() {
   if ( region.compareTo("EUR") == 0 ) {
     setUSA();
   }
@@ -96,23 +157,75 @@ void cambiarRegion() {
   }
 }
 
+/**
+ * Varia la fecuencia de refresco (50 - 60Hz) sin variar la zona del idioma, 
+ * de forma que, si se corresponde con otra de las 3 regiones, cambia a una de
+ * ellas o, en otro caso, activa la region "custom" (japones, 50Hz)
+ *
+ * @param ---
+ * @return void
+ */
+void cambiarFrecuencia() {
+  if ( region.compareTo("EUR") == 0 ) {
+    setUSA();
+  }
+  else if ( region.compareTo("USA") == 0 )
+  {
+    setEUR();
+  }
+  else if( region.compareTo("JAP") == 0 ) {
+    setCustom();
+  }
+  else {
+    setJAP();
+  }
+}
+
+/**
+ * Esta funcion no hace nada, pero es necesario tenerla para salir del modo de
+ * bajo consumo (sleep mode) en el que ponemos el arduino hasta que se pulsa
+ * reset en la consola
+ *
+ * @param ---
+ * @return void
+ */
 void pulsarReset() {
   // No hay que hacer nada, solo lo usamos para salir de low power
 }
 
+/**
+ * Ejecuta la accion de pulsar el reset en la Mega Drive II, poniendo durante
+ * un corto espacio de tiempo la linea de reset en estado de alta impedancia
+ * 
+ * @param ---
+ * @return void
+ */
 void reinicarMD() {
   // TODO: No implementado
 }
 
+/**
+ * Controla la pulsacion del boton de reset, determinando, segun el tiempo, si 
+ * se debe resetear la consola, cambiar la frecuencia o cambiar la region
+ * 
+ * @param ---
+ * @return void
+ */
 void controlarReset() {
-  inicioPulsacion = millis();
+  int inicioPulsacion = millis();
+  int transcurrido = 0;
 
   while ( digitalRead(resetInput) == HIGH ) {
     // No es necesario hacer nada
   }
 
-  if ( (millis() - inicioPulsacion) >= 2000 ) {
+  transcurrido = millis() - inicioPulsacion; 
+
+  if ( transcurrido >= 2000 ) {
     cambiarRegion();
+  }
+  else if ( transcurrido >= 1000 ) {
+    cambiarFrecuencia();
   }
   else {
     reinicarMD(); // No implementado
@@ -130,8 +243,7 @@ void setup() {
   pinMode(gled, OUTPUT);
   pinMode(resetInput, INPUT);
 
-  // Por defecto la region original al encender el PAL Europeo
-  setEUR();
+  setEUR(); // Por defecto la region original al encender el PAL Europeo
 }
 
 void loop() {
@@ -141,8 +253,7 @@ void loop() {
   // Entramos en modo sleep hasta la interrupcion
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 
-  // Fin de la interrupcion
-  detachInterrupt(0); 
+  detachInterrupt(0); // Fin de la interrupcion 
 
-  controlarReset();
+  controlarReset(); // Controlamos la accion a realizar
 }
